435ab3c4e9fb7648840453b8a2f320911cd34eb4
wgt75 <mark11seM>
2022-04-29 18:21:04 +0700

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path");
 const app = express()
 
 app.get("/", function (req, res) {
@@ -6,3 +7,11 @@ app.get("/", function (req, res) {
 })
 
 app.listen(process.env.PORT || 5000)
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../dist')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../dist/main.html'))
+})
