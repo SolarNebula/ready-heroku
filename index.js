365ef15c6ac35f7cc44dc913a342d906a1825f45
wgt75 <mark11seM>
2022-04-29 18:52:36 +0700

    make it better

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require("express")
 const path = require("path");
 const app = express()
 
-app.get("/", function (req, res) {
-    res.send("ok")
-})
+// app.get("/", function (req, res) {
+//     res.send("ok")
+// })
 
 app.listen(process.env.PORT || 5000)
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../dist')))
+app.use(express.static(path.join(__dirname, '../CSS')))
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../dist/main.html'))
-})
+
+app.use(express.static("dist"))
+app.get('/', (req, res) => res.sendFile('dist/main.html', { root: __dirname }));
+app.get('/', (req, res) => res.sendFile('CSS/main.css', { root: __dirname }));
+
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/../dist/main.html'))
+// })
